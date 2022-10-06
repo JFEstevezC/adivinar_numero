63fd7b22e34779e6bc909faf59100846ba2aafc3
JFEstevezC <jose2224646@correo.uis.edu.co>
2022-10-06 06:38:04 -0500

    Diagrama de flujo

--- adivinar_numero.py
+++ adivinar_numero.py
@@ -3,15 +3,16 @@ print("------------------------------------------")
 print("--------------ADIVINA-NÚMERO--------------")
 print("------------------------------------------")
 
+i = 0
 ale=random.randint(0,10000)
 n=int(input("Digite un número entre 0 y 10000: "))
-i = 0
-if n>=0 and n<=10000:
+
+if n >= 0 and n <= 10000:
     while n != ale:
         if n > ale:
-            print("INCORRECTO: Intente con un número menor")
+            print("Incorrecto, el número es menor")
         else:
-            print("INCORRECTO: Intente con un número mayor")
+            print("Incorrecto, el número es mayor")
 
         n=int(input("Digite otro número entre 0 y 10000: "))
         i = i + 1
